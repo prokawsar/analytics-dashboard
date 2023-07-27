5e8f014617b045a3ed9b96f3f7e9f42ab9880e33
Kawsar Ahmed <mkawsar10@gmail.com>
2023-07-27 15:50:05 +0600

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Dashboard starter</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline mx-auto container flex justify-center">Hello world!</h1>
