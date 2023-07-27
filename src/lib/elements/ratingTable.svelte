39a53a547962c994bf409591e38e6162330ff740
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-27 19:44:40 +0600

    sample data

--- src/lib/elements/ratingTable.svelte
+++ src/lib/elements/ratingTable.svelte
@@ -1,3 +1,34 @@
+<script>
+	const sampleLocationData = [
+		{
+			'Location name': 'Central Park, New York',
+			'Google Rating': 4.8,
+			'Total Reviews': 2500
+		},
+		{
+			'Location name': 'Eiffel Tower, Paris',
+			'Google Rating': 4.6,
+			'Total Reviews': 1800
+		},
+		{
+			'Location name': 'Big Ben, London',
+			'Google Rating': 4.5,
+			'Total Reviews': 2100
+		},
+		{
+			'Location name': 'Tokyo Tower, Tokyo',
+			'Google Rating': 4.9,
+			'Total Reviews': 3200
+		},
+		{
+			'Location name': 'Brandenburg Gate, Berlin',
+			'Google Rating': 4.4,
+			'Total Reviews': 1400
+		}
+		// Add more data objects here if needed
+	];
+</script>
+
 <table class="w-full text-left mt-2">
 	<thead>
 		<th class="text-xs text-gray-400"> Location name </th>
@@ -5,10 +36,12 @@
 		<th class="text-xs text-gray-400"> Total Reviews </th>
 	</thead>
 	<tbody>
-		<tr>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-		</tr>
+		{#each sampleLocationData as data}
+			<tr>
+				<td class="text-xs"> {data['Location name']} </td>
+				<td class="text-xs"> {data['Google Rating']} </td>
+				<td class="text-xs"> {data['Total Reviews']} </td>
+			</tr>
+		{/each}
 	</tbody>
 </table>
--- src/lib/elements/reviewsTable.svelte
+++ src/lib/elements/reviewsTable.svelte
@@ -1,3 +1,48 @@
+<script>
+	const sampleData = [
+		{
+			'Review Date': '2023-07-15',
+			Location: 'New York',
+			'Profile Image': 'profile_images/user1.jpg',
+			'Display Name': 'John Doe',
+			Rating: 4.5,
+			Reviews: 25
+		},
+		{
+			'Review Date': '2023-07-17',
+			Location: 'London',
+			'Profile Image': 'profile_images/user2.jpg',
+			'Display Name': 'Jane Smith',
+			Rating: 3.8,
+			Reviews: 12
+		},
+		{
+			'Review Date': '2023-07-20',
+			Location: 'Paris',
+			'Profile Image': 'profile_images/user3.jpg',
+			'Display Name': 'Michael Johnson',
+			Rating: 4.2,
+			Reviews: 18
+		},
+		{
+			'Review Date': '2023-07-22',
+			Location: 'Tokyo',
+			'Profile Image': 'profile_images/user4.jpg',
+			'Display Name': 'Samantha Lee',
+			Rating: 4.9,
+			Reviews: 37
+		},
+		{
+			'Review Date': '2023-07-25',
+			Location: 'Berlin',
+			'Profile Image': 'profile_images/user5.jpg',
+			'Display Name': 'David Brown',
+			Rating: 4.0,
+			Reviews: 9
+		}
+	];
+</script>
+
 <table class="w-full text-left mt-2">
 	<thead>
 		<th class="text-xs text-gray-400"> Review Date </th>
@@ -8,13 +53,15 @@
 		<th class="text-xs text-gray-400"> Reviews </th>
 	</thead>
 	<tbody>
-		<tr>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-			<td class="text-xs"> Canada </td>
-		</tr>
+		{#each sampleData as data}
+			<tr class="">
+				<td class="text-xs"> {data['Review Date']} </td>
+				<td class="text-xs"> {data.Location}</td>
+				<td class="text-xs">{data['Profile Image']} </td>
+				<td class="text-xs"> {data['Display Name']} </td>
+				<td class="text-xs"> {data.Rating} </td>
+				<td class="text-xs"> {data.Reviews} </td>
+			</tr>
+		{/each}
 	</tbody>
 </table>
