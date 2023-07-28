e36ab3eca2b353e9b2b21603109f97d1b6703d80
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-28 11:53:57 +0600

    responsive nav

--- src/lib/elements/nav.svelte
+++ src/lib/elements/nav.svelte
@@ -8,11 +8,10 @@
 </script>
 
 <nav
-	class="top-0 left-0 w-full shadow-md shadow-gray-600 z-10 bg-gray-700 md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
+	class="top-0 left-0 w-full shadow-md shadow-gray-600 z-10 bg-gray-700 md:flex-row md:flex-nowrap md:justify-start flex items-center p-3 md:p-4">
 	<div
 		class="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
-		<a class=" text-sm uppercase text-white hidden lg:inline-block font-semibold" href="/"
-			>Dashboard</a>
+		<a class=" text-sm uppercase text-white inline-block font-semibold" href="/">Dashboard</a>
 		<form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
 			<div class="relative flex w-full flex-wrap items-stretch">
 				<span
@@ -26,7 +25,7 @@
 					class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10" />
 			</div>
 		</form>
-		<ul class="flex-col md:flex-row gap-4 list-none items-center hidden md:flex">
+		<ul class="md:flex-row gap-4 list-none items-center flex flex-row">
 			<div>
 				<a
 					class="cursor-pointer h-12 w-12 flex justify-center items-center"
@@ -36,10 +35,10 @@
 				<Notification bind:show={showNotification} />
 			</div>
 			<div>
-				<a class="text-blueGray-500 block" href="#" on:click={() => (openList = !openList)}>
+				<a class="block" href="#" on:click={() => (openList = !openList)}>
 					<div class="items-center flex">
 						<span
-							class="w-12 h-12 text-sm text-white bg-blueGray-200 inline-flex items-center justify-center rounded-full">
+							class="w-12 h-12 text-sm text-white inline-flex items-center justify-center rounded-full">
 							<img
 								alt="..."
 								class="w-full rounded-full align-middle border-none shadow-lg"
