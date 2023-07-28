35c9ebd4056a7b30f833a1c7829ab881a7daa732
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-28 11:33:31 +0600

    hover outline

--- src/lib/elements/stats.svelte
+++ src/lib/elements/stats.svelte
@@ -9,7 +9,8 @@
 	$: sign = changeValue > 0 ? '+' : '-';
 </script>
 
-<div class="py-10 px-12 text-center rounded-md bg-gray-600 text-white flex flex-col grow">
+<div
+	class="hover:outline outline-1 outline-blue-400 py-10 px-12 text-center rounded-md bg-gray-600 text-white flex flex-col grow">
 	<p class="text-3xl font-bold">{rating}</p>
 	<p class="text-sm font-bold">{title}</p>
 	{#if type}
